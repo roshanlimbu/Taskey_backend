e54049e8ed4d1a6f05dc621f6bab66134f29a7c4
roshanlimbu <topazkayle@gmail.com>
2025-05-18 21:44:12 +0545

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // Role constants
+    const ROLE_SUPER_ADMIN = 1;
+    const ROLE_ADMIN = 2;
+    const ROLE_USER = 3;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -22,6 +27,7 @@ class User extends Authenticatable
         'email',
         'github_token',
         'github_refresh_token',
+        'role', // Added role to fillable
     ];
 
     /**
@@ -42,5 +48,36 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => 'integer', // Cast role as integer
     ];
+
+    /**
+     * Check if the user is a super admin.
+     *
+     * @return bool
+     */
+    public function isSuperAdmin()
+    {
+        return $this->role === self::ROLE_SUPER_ADMIN;
+    }
+
+    /**
+     * Check if the user is an admin.
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
+
+    /**
+     * Check if the user is a regular user.
+     *
+     * @return bool
+     */
+    public function isUser()
+    {
+        return $this->role === self::ROLE_USER;
+    }
 }
