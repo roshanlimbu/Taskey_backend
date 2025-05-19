b10b55ef76a708dc5e8b9f34e3cde45c4c3156fb
roshanlimbu <topazkayle@gmail.com>
2025-05-19 22:56:59 +0545

    feat: add project management routes with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,13 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Sadmin\projectController;
 
+// Project routes
+Route::prefix('sadmin')->middleware('auth:api')->group(function () {
+    Route::post('/projects', [projectController::class, 'createProject']);
+    Route::get('/projects', [projectController::class, 'index']);
+    Route::post('/projects/{projectId}/members', [projectController::class, 'addMembers']);
+    Route::post('/projects/{projectId}/tasks', [projectController::class, 'createTask']);
+    Route::post('/tasks/{taskId}/assign', [projectController::class, 'assignTask']);
+});
