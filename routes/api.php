557e330159aa3bab3e05f3e914c5b63e03f8a14c
roshanlimbu <topazkayle@gmail.com>
2025-05-21 23:46:15 +0545

    feat: add task management routes to TaskController for creating, updating, and deleting tasks

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Sadmin\projectController;
+use App\Http\Controllers\Sadmin\TaskController;
 
 // Project routes
 Route::prefix('sadmin')->middleware('auth:api')->group(function () {
@@ -12,4 +13,12 @@ Route::prefix('sadmin')->middleware('auth:api')->group(function () {
     Route::post('/tasks/{taskId}/assign', [projectController::class, 'assignTask']);
     Route::put('/projects/{projectId}', [projectController::class, 'editProject']);
     Route::delete('/projects/{projectId}', [projectController::class, 'deleteProject']);
+    Route::get('/projects/{id}', [projectController::class, 'show']);
+    // Task routes
+    Route::post('/projects/{projectId}/tasks', [TaskController::class, 'addTask']);
+    Route::put('/tasks/{taskId}', [TaskController::class, 'editTask']);
+    Route::put('/tasks/{taskId}/status', [TaskController::class, 'updateTaskStatus']);
+    Route::delete('/tasks/{taskId}', [TaskController::class, 'deleteTask']);
+    Route::post('/tasks/{taskId}/assign', [TaskController::class, 'assignTask']);
+    Route::post('/tasks/{taskId}/remove-user', [TaskController::class, 'removeUserFromTask']);
 });
