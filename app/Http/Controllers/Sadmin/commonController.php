5fad0b010b8b14df577a1d1ae971dc31cc48fa96
roshanlimbu <topazkayle@gmail.com>
2025-05-25 08:14:28 +0545

    refactor: clean up commonController by removing unnecessary whitespace

--- app/Http/Controllers/Sadmin/commonController.php
+++ app/Http/Controllers/Sadmin/commonController.php
@@ -4,17 +4,14 @@ namespace App\Http\Controllers\sadmin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class commonController extends Controller
 {
-    
+
     //  get all users
     public function getAllUsers()
     {
         $users = User::all();
         return response()->json($users);
     }
-
-    
 }
