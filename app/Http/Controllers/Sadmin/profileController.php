8f73a2d98e5118469ff0d00aa164e34cd36cc2ef
roshanlimbu <topazkayle@gmail.com>
2025-05-25 08:14:43 +0545

    feat: create profileController to handle user profile updates with validation for github_id, dev_role, and role

---
+++ app/Http/Controllers/Sadmin/profileController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Sadmin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class profileController extends Controller
+{
+
+    public function updateProfile(Request $request) {
+        $validated = $request->validate([
+            'github_id' => 'required|exists:users,github_id',
+            'dev_role'=> 'required|string|max:255',
+            'role' => 'required|integer|in:1,2,3',
+        ]);
+
+        $user = User::find($validated['github_id']);
+        $user->dev_role = $validated['dev_role'];
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+        ]);
+    }
+}
