638f1220cd49c686ff7953e3c9806494578bc9f1
roshanlimbu <topazkayle@gmail.com>
2025-05-26 18:33:34 +0545

    feat: create activities model to manage user activity logs with relationships to user, project, and task

---
+++ app/Models/activities.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class activities extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'project_id',
+        'task_id',
+        'type',
+        'title',
+        'description',
+        'meta'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
