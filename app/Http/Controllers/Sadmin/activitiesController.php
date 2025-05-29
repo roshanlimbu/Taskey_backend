acbaa7aa79edc203c861774ed4faf959560ce009
roshanlimbu <topazkayle@gmail.com>
2025-05-29 16:12:53 +0545

    feat: added other contorller func

--- app/Http/Controllers/Sadmin/activitiesController.php
+++ app/Http/Controllers/Sadmin/activitiesController.php
@@ -8,13 +8,40 @@ use App\Models\activities;
 
 class activitiesController extends Controller
 {
+
+
+    public function getAllActivities()
+    {
+        $activities = activities::with('user')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return response()->json(['activities' => $activities]);
+    }
+
+
+
     public function activities()
     {
         $activities = activities::with('user')
             ->orderBy('created_at', 'desc')
-            ->limit(10)
+            ->limit(5)
             ->get();
 
         return response()->json(['activities' => $activities]);
+    }
+
+
+    public function deleteActivity($id)
+    {
+        $activity = activities::find($id);
+
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+
+        $activity->delete();
+
+        return response()->json(['message' => 'Activity deleted successfully']);
     }
 }
