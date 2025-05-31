5da3e83e8646bbd517477121407770c5beb92753
roshanlimbu <topazkayle@gmail.com>
2025-05-31 21:38:26 +0545

    feat: ativities controller

--- app/Http/Controllers/Sadmin/activitiesController.php
+++ app/Http/Controllers/Sadmin/activitiesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Sadmin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\activities;
+use Illuminate\Support\Facades\Auth;
 
 class activitiesController extends Controller
 {
@@ -43,5 +44,31 @@ class activitiesController extends Controller
         $activity->delete();
 
         return response()->json(['message' => 'Activity deleted successfully']);
+    }
+
+
+    public function commentOnActivity(Request $request)
+    {
+        $validator = validator($request->all(), [
+            'activity_id' => 'required|exists:activities,id',
+            'comment' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return res(false, [], $validator->errors()->all());
+        }
+
+        $activity = activities::find($request->activity_id);
+        $activity->comments = json_encode([
+            'user_id' => Auth::user()->id,
+            'comment' => $request->comment,
+        ]);
+        $activity->save();
+        return res(true, [], ['Comment added successfully']);
+    }
+    public function getComments($id)
+    {
+        $comments = activities::find($id)->comments;
+        return res(true, $comments, ['Comments fetched successfully']);
     }
 }
