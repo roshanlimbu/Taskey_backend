bbbf6f4a2194dad7b47373ad41bde65f64260879
roshanlimbu <topazkayle@gmail.com>
2025-06-01 18:48:35 +0545

    feat: create Reports model for managing project reports with fillable attributes

---
+++ app/Models/Reports.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reports extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'user_id',
+        'title',
+        'report',
+    ];
+}
